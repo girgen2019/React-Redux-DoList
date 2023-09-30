18b225415c40e776d06b38570dca3d1a4bd6a721
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2023-09-30 03:12:04 +0300

    add imports in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "antd/dist/reset.css"
+import { Provider } from 'react-redux';
+import { store } from './Components/store/store';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
