4e0e312c3ee8d0fa6b5790d2d06892ed53cd356c
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2023-11-03 15:21:37 +0300

    fix TodoList.tsx

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -8,38 +8,33 @@ import { Button, Input } from 'antd';
 import { useDispatch } from 'react-redux';
 import { editTodo } from './store/todoSlice';
 import { title } from 'process';
-import { useRef } from 'react';
-
 
 interface ITodoList {
-  todos: any ;
+  todos: any;
   setTodos: any;
   deleteTodo: (index: string) => void;
 }
 
 const TodoList = ({ todos, deleteTodo, setTodos }: ITodoList) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [editText, setEditText] = useState<number | null | string>(null);
   const [value, setValue] = useState<string>('');
-  const valueRef = useRef<string>()
-  
 
   const editTodoValue = (id: number, todo: string) => {
     setEditText(id);
     setValue(todo);
-
   };
 
-  const saveTodo = (id: string, value:string) => {
-   dispatch(editTodo({id, value}))
-   setValue(value)
-   setEditText(null);
+  const saveTodo = (id: string, value: string) => {
+    dispatch(editTodo({ id, value }));
+    setValue(value);
+    setEditText(null);
   };
 
   return (
     <div className={styles.todoListContainer}>
       {!todos.length && <h2>Todo list is empty</h2>}
-      {todos.map((todo:any, index:any) =>
+      {todos.map((todo: any, index: any) =>
         editText == todo.id ? (
           <div className={styless.todo}>
             <div>
@@ -51,15 +46,15 @@ const TodoList = ({ todos, deleteTodo, setTodos }: ITodoList) => {
           </div>
         ) : (
           <Todo
-          key={todo.id}
-          todo={todo.title}
-          index={todo.id}
-          deleteTodo={deleteTodo}
-          editTodo={editTodoValue}
-          value={value}
+            key={todo.id}
+            todo={todo.title}
+            index={todo.id}
+            deleteTodo={deleteTodo}
+            editTodo={editTodoValue}
+            value={value}
           />
-          )
-          )}
+        )
+      )}
     </div>
   );
 };
